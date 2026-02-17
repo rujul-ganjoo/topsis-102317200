392b423c6e3e5415e7f9d8e8b095b0c3fd03fdf7
rujul-ganjoo <142878606+rujul-ganjoo@users.noreply.github.com>
2026-02-17 22:31:41 +0530

    Update api.js

--- topsis-102317200/Web/client/src/services/api.js
+++ topsis-102317200/Web/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://topsis-3s7c.vercel.app/api",
+  baseURL: "https://topsis-102317200-k28h.vercel.app/api",
 });
 
 export default api;
